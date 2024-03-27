e47e4e3cddc9265a24db4fd45e2137811db7ff05
Shraddha-T <shraddhathorbole15@gmail.com>
2024-03-27 15:05:45 +0530

    feat:Finalize Method

--- Finalize.java
+++ Finalize.java
@@ -3,14 +3,14 @@ public class Finalize
     int i = 50;
     protected void finalize() throws Throwable
     {
-        System.out.println("From Finalize Method");
+        System.out.println("Finalize Method");
     }
     public static void main(String[] args)
     {
         Finalize F1 = new Finalize();
         Finalize F2 = new Finalize();
         F1 = F2;
-        System.out.println("done");
+        System.out.println("Done!!");
         System.gc(); 
     }
 }
