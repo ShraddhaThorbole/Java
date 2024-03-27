6461161c139a611722181b672e140ea99f5be7c8
Shraddha-T <shraddhathorbole15@gmail.com>
2024-03-27 20:12:49 +0530

    feat:Single Inheritance

---
+++ Single_Inheritance.java
@@ -0,0 +1,51 @@
+import java.util.*;
+class Department
+{
+	String dept_name;
+	int dept_no;
+	void getDept()
+	{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter Department Name:");
+	dept_name=sc.nextLine();
+	System.out.println("Enter Department No.:");
+	dept_no=Integer.parseInt(sc.nextLine());
+	}
+}
+class Student extends Department
+{
+	String s_name;
+	int r_no;
+	void getStud()
+	{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter student name:");
+	s_name=sc.nextLine();
+	System.out.println("Enter student no.:");
+	r_no=Integer.parseInt(sc.nextLine());
+	}
+	void putData()
+	{
+		System.out.println("Department name:"+dept_name+"Department No."+dept_no+"Student Name:"+s_name+"Student Roll no. :"+r_no);
+	}
+}
+
+class Single_Inheritance
+{
+	public static void main(String args[])
+	{
+		Student s=new Student();
+		s.getDept();
+		s.getStud();
+		s.putData();
+	}
+}
+
+
+
+
+
+
+
+
+
